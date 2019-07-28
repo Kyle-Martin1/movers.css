44badbdfd751b0edf9f7005b082deb060bcc2ced
Kyle Martin <kylemartin1403@hotmail.co.uk>
2019-07-28 10:57:06 +0100

    Updates to site build - added overlay menu

--- docs/moversscript.js
+++ docs/moversscript.js
@@ -1,8 +1,28 @@
 ////////// Custom Cursor
 var cursor = document.querySelector('.cursor');
 
-  document.addEventListener('mousemove', function(e) {
+    document.addEventListener('mousemove', function(e) {
 
-    cursor.setAttribute('style', 'top: ' + (e.pageY - 5) + 'px;' + 'left: ' + (e.pageX - 5) + 'px;');
+      cursor.setAttribute('style', 'top: ' + (e.pageY - 5) + 'px;' + 'left: ' + (e.pageX - 5) + 'px;');
+
+});
+
+
+
+
+////////// Overlay Menu
+document.querySelector('.openClose').addEventListener('click', () => {
+
+    document.querySelector('.navOverlay').classList.toggle('open');
+    document.querySelector('.specialAnimationsLink').style.visibility = "visible";
+    document.querySelector('.entryAnimationsLink').style.visibility = "visible";
+    document.querySelector('.exitAnimationsLink').style.visibility = "visible";
+    document.querySelector('.githubLink').style.visibility = "visible";
+
+});
+
+document.querySelector('.navLinks').addEventListener('click', () => {
+
+    document.querySelector('.navOverlay').classList.toggle('open');
 
 });
